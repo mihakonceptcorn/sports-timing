2703b0438ef67e508631fd5b4a7e7767e0d4dab8
mihakonceptcorn <skinzzadmin@gmail.com>
2022-12-19 19:58:48 +0200

    jwt

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,3 @@
-'use strict'
-
 const passport = require("passport");
 const usersController = require("../controllers/UserController");
 module.exports = (app) => {
--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const bodyParser = require('body-parser')
 const app = express()
 const PORT = process.env.PORT || 3000
 const passport = require('passport')
+const userRoutes = require('./routes/users')
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(passport.initialize())
 require('./middleware/passport')(passport)
 
-const routes = require('./settings/routes')
-routes(app)
+userRoutes(app)
 
 app.listen(PORT, () => console.log(`Server has been started on port ${PORT}...`))
