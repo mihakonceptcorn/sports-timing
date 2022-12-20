f238a4ccf1f29109a1ba35ea2de639024dbc842c
mihakonceptcorn <skinzzadmin@gmail.com>
2022-12-20 22:08:41 +0200

    cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
+var cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 const PORT = process.env.PORT || 3000
 const passport = require('passport')
 const userRoutes = require('./routes/users')
 
+app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(passport.initialize())
