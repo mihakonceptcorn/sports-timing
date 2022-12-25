9515c4c5a5c871e027cb0d163e49c1073dd78db0
mihakonceptcorn <skinzzadmin@gmail.com>
2022-12-25 13:15:37 +0200

    db pool

--- settings/db.js
+++ settings/db.js
@@ -1,20 +1,33 @@
 const mysql = require('mysql')
 const config = require('./config')
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: config.HOST,
   port: config.PORT,
   user: config.DBUSER,
   password: config.DBPASSWORD,
-  database: config.DBNAME
+  database: config.DBNAME,
+  connectionLimit: 10
 })
 
-connection.connect((error) => {
-  if (error) {
-    return console.log('DataBase connection error:', error)
-  } else {
-    return console.log('DataBase connection success!')
-  }
+connection.on('connect', function () {
+  return console.log('DB Connection established')
 })
+
+connection.on('error', function (err) {
+  return console.error(new Date(), 'MySQL error', err.code)
+})
+
+connection.on('close', function (err) {
+  return console.error(new Date(), 'MySQL close', err)
+})
+
+// connection.connect((error) => {
+//   if (error) {
+//     return console.log('DataBase connection error:', error)
+//   } else {
+//     return console.log('DataBase connection success!')
+//   }
+// })
 
 module.exports = connection
