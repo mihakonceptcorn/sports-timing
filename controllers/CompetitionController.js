d91e1f4618c9d2e7301249299f1535fe209a989a
mihakonceptcorn <skinzzadmin@gmail.com>
2023-01-07 12:06:44 +0200

    comp fields

--- controllers/CompetitionController.js
+++ controllers/CompetitionController.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs")
 exports.getCompetitions = (req, res) => {
   const limit = req.params.limit || 20
   const skip = req.params.skip || 0
-  const sql = 'SELECT `id`, `name`, `date`, `stages`, `rounds`, `finished` FROM `competitions` LIMIT ' + limit + ' OFFSET ' + skip + ''
+  const sql = 'SELECT `id`, `name`, `date`, `stages`, `rounds`, `finished`, `country`, `city`, `location`, `description` FROM `competitions` LIMIT ' + limit + ' OFFSET ' + skip + ''
   db.query(sql, (error, rows, fields) => {
     if (error) {
       response.status(400, error, res)
@@ -16,7 +16,7 @@ exports.getCompetitions = (req, res) => {
 }
 
 exports.getCompetitionById = (req, res) => {
-  const sql = "SELECT `id`, `name`, `date`, `stages`, `rounds`, `finished` FROM `competitions` WHERE `id` = '" + req.params.id + "'"
+  const sql = "SELECT `id`, `name`, `date`, `stages`, `rounds`, `finished`, `country`, `city`, `location`, `description` FROM `competitions` WHERE `id` = '" + req.params.id + "'"
   db.query(sql, (error, rows, fields) => {
     if (error) {
       response.status(400, error, res)
@@ -31,8 +31,13 @@ exports.createCompetition = (req, res) => {
   const date = req.body.date
   const stages = req.body.stages
   const rounds = req.body.rounds
+  const country = req.body.country
+  const city = req.body.city
+  const location = req.body.location
+  const description = req.body.description
 
-  const sql = "INSERT INTO `competitions` (`name`, `date`, `stages`, `rounds`) VALUES('" + name + "', '" + date + "', '" + stages + "', '" + rounds + "')"
+  const sql = "INSERT INTO `competitions` (`name`, `date`, `stages`, `rounds`, `country`, `city`, `location`, `description`)" +
+    "VALUES('" + name + "', '" + date + "', '" + stages + "', '" + rounds + "', '" + country + "', '" + city + "', '" + location + "', '" + description + "')"
 
   db.query(sql, (error, results) => {
     if (error) {
